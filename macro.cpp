3bd0e04fd95f2fc6ed525e61558063d220a9d38f
Shanta Shil <121443679+ShantoOBS@users.noreply.github.com>
2024-11-27 00:49:16 +0600

    Create macro.cpp

---
+++ macro.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+
+    What is Macro?
+    -Macro is a peice of code in a program which is given some name. Whenever the nae is used it is replaced by the contents of the macro
+
+    Note:- Macro is define by the help of #define 
+
+    Syntax:- #define macro_name content
+   
+ */
+
+// Macros
+#define ff first
+#define ss second
+#define endl "\n"
+#define pb emplace_back
+#define mp(x,y) make_pair((x),(y))
+#define yes cout<<"YES\n";
+#define no cout<<"NO\n";
+#define pimp cout<<"IMPOSSIBLE\n";
+#define mid(s,e) ((s+e)/2)
+#define forl(n) for(long long i=0;i<(n);i++)
+
+int main() {
+     
+     int a,b;
+     cin>> a>>b;
+
+    cout<<mid(a,b)<<endl;
+     
+     
+}
