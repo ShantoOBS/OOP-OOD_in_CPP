e1933d603f17871b7c264c521deca9a9bb8fa52d
Shanta Shil <121443679+ShantoOBS@users.noreply.github.com>
2024-11-27 02:58:22 +0600

    Create Area of a Rectangle.cpp

---
+++ Oops_Practice_coding_problems/Area of a Rectangle.cpp
@@ -0,0 +1,14 @@
+
+// Coding Ninjas Link : https://www.naukri.com/code360/guided-paths/oops-in-c/content/274160/offering/3757750?leftPanelTabValue=SUBMISSION&customSource=studio_nav
+
+#include <bits/stdc++.h> 
+class Rectangle {
+    // Write your code here.
+    public:
+    int length;
+    int breadth;
+
+    int getArea(){
+        return length*breadth;
+    }
+};
