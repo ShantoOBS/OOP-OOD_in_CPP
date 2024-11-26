715224ee111d077591ef9c0daba2aea83035e55e
Shanta Shil <121443679+ShantoOBS@users.noreply.github.com>
2024-11-27 00:09:27 +0600

    Create Padding_and_Greedy_Alignment.cpp

---
+++ Padding_and_Greedy_Alignment.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+              //https://www.javatpoint.com/structure-padding-in-c
+class A {
+};
+
+int main()
+{
+    printf("Size of class A: %lu\n", sizeof(A));
+    return 0;
+}
