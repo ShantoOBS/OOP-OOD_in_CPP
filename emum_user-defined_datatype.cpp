5c8dbadca6d9c70b223709b6d06422eb143317af
Shanta Shil <121443679+ShantoOBS@users.noreply.github.com>
2024-11-27 00:34:13 +0600

    Create emum_user-defined_datatype.cpp

---
+++ emum_user-defined_datatype.cpp
@@ -0,0 +1,24 @@
+// Online C++ compiler to run C++ program online
+#include <bits/stdc++.h>
+using namespace std;
+ /* 
+    What is enum?
+   -enum is a user defined name that consists fo integer constents
+  syntax : enum {value1, value2, value3,value4 };
+
+*/
+ 
+int main() {
+     
+     enum gender{
+          male, female, others //0 1 2
+     };
+     
+     cout<<male<<endl;
+     gender str=others;
+     cout<<str<<endl;
+     
+     return 0;
+     
+     
+}
